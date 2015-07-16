859f0c12162fc6c0cc11f9dc52c32ee32a75898f
antkahn <ant.kahn@gmail.com>
2015-07-16 11:53:14 +0200

    modif

--- web/config.php
+++ web/config.php
@@ -34,7 +34,7 @@ $minorProblems = $symfonyRequirements->getFailedRecommendations();
         <div id="content">
             <div class="header clear-fix">
                 <div class="header-logo">
-                    <img src="bundles/framework/images/logo_symfony.png" alt="Symfony" />
+                    <img src="http://www.theodo.fr/bundles/theodowebsite/images/theodo-logo-medium.png" alt="Symfony" />
                 </div>
 
                 <div class="search">
