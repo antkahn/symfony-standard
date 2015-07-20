3c32d0ec0ff77b202ddda6710b36635823666bfc
antkahn <ant.kahn@gmail.com>
2015-07-20 12:55:09 +0200

    add entity on site

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -9,9 +9,21 @@ class DefaultController extends Controller
 {
     public function indexAction($name, $count)
     {
-        return $this->render(
-                'EventBundle:Default:index.html.twig',
-                array('name' => $name,'count' => $count)
-        );
+    	// $em = $this->container->get('doctrine')->getManager();
+    	$em = $this->getDoctrine()->getManager();
+    	$repo = $em->getRepository('EventBundle:Event');
+
+        $event = $repo->findOneBy(array(
+	        'name' => 'Darth\'s surprise birthday party',
+	    ));
+
+	    return $this->render(
+	        'EventBundle:Default:index.html.twig',
+	        array(
+	            'name' => $name,
+	            'count' => $count,
+	            'event'=> $event,
+	        )
+	    );
     }
 }
